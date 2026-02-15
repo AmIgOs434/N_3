91e516caa2a9551dfba848a1facfe42b4daa26c5
AmIgOs434 <104652966+AmIgOs434@users.noreply.github.com>
2026-02-15 18:00:03 +0300

    Change host from '0.0.0.0' to '127.0.0.1'

--- app.py
+++ app.py
@@ -435,7 +435,7 @@ if __name__ == "__main__":
     
     uvicorn.run(
         "app:app",
-        host="0.0.0.0",
+        host="127.0.0.1",
         port=port,
         reload=False,
         log_level="info"
